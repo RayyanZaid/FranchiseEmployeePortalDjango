d5ebb71d136ddcf7abd56b2ecd3556c2e9d39258
RayyanZaid <rayyanzaid0401@gmail.com>
2023-10-07 02:24:01 -0700

    1. Create a view function 2. Add it to a URL Configuration

---
+++ playground/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, URLPattern
+from . import views # from the current folder
+from typing import List
+
+# array of URLPatterns
+# URL Configuration
+urlpatterns : List[URLPattern]  = [
+
+         # the url the user has to access       # the view that is run (reference to the function)
+    path(      'playground/hello',               views.say_hello)
+
+]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# View = request handler
+
+# Map each view to a URL. So when you get a request at that URL, this function is called
+def say_hello(requestObject): 
+    # can pull data from DB in here
+
+
+    return HttpResponse('Hello World')
