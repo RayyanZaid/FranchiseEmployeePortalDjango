fb3e85e71b24b1627af82e9c79ded83b825bbdbe
RayyanZaid <rayyanzaid0401@gmail.com>
2023-10-07 12:02:34 -0700

    can be accessed by mobile

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 # View = request handler
 
 # Map each view to a URL. So when you get a request at that URL, this function is called
-def say_hello(requestObject): 
-    # can pull data from DB in here
 
 
-    return HttpResponse('Hello World')
+def say_hello(request):
+    # Create a Python dictionary representing your JSON data
+    data = {'message': 'Hello, World!'}
+
+    # Use JsonResponse to convert the dictionary to a JSON response
+    return JsonResponse(data)
